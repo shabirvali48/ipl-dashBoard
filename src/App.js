ba6081208fb3ba69712d382668bcf5404e13144c
shaik shbirvali <shabirvali48@gmail.com>
2024-07-25 18:23:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {  Routes, Route } from 'react-router-dom';
+import './App.css'
+import Home from './components/Home'
+import TeamMatches from './components/TeamMatches'
+import NotFound from './components/NotFound'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    
+<>
+<Routes>
+    <Route exact path="/" component={Home} />
+    <Route path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Routes>
+</>
+    
   );
+ 
 }
-
-export default App;
+export default App
---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div className='text-center'>
+        <div class="spinner-border text-primary"></div>
+
+    </div>
+  )
+}
